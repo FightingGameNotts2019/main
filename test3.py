10357221735cdff38be6d0b647c6fb4dd0733e01
sromanov409 <57843492+sromanov409@users.noreply.github.com>
2019-11-30 16:40:50 +0000

    Add files via upload
    Jumping works. The collision is weird.

--- test3.py
+++ test3.py
@@ -1,14 +1,17 @@
 import pygame
-
+#Still not fixed these, leave them for now.
 WIDTH = 1600
 HEIGHT = 800
 PLAYER_HEIGHT = 20
 PLAYER_WIDTH = 59
+
 FPS = 60
+#Player life
 LIFE = 200
-#might want to adjust the speeds on mac
-FALLING_SPEED = 1
-MOVING_SPEED = 1
+TAKE_LIFE = 20
+#Actions, these are good I found and because we have the FPS they should be consistent
+GRAVITY = 7
+MOVING_SPEED = 5
 #Not used yet
 JUMP_HEIGHT = 100
 PLAYER1_X = 500
@@ -20,17 +23,27 @@ clock = pygame.time.Clock()
 
 
 class Player():
-    
+    #Probably need to add separate colours for each player, but then aagin we have the sprites?
     def __init__(self, x,y):
-        #This is player number, ie 1 or 2
+        #These are not used but we still need then for now.
         self.x = x
         self.y = y
+        #the life rectangles for each player
         self.life = pygame.Rect(100,100,LIFE,30)
         self.life2 = pygame.Rect(1300,100,LIFE,30)
+        #PLayer hitboxes
         self.hitbox = pygame.Rect(self.x,self.y,PLAYER_HEIGHT,PLAYER_WIDTH)
         self.hitbox2 = pygame.Rect(self.x+400,self.y,PLAYER_HEIGHT,PLAYER_WIDTH)
+        #Used in the jump function.
+        self.jump1 = False
+        self.jump2 = False
+        self.jumpCount = JUMP_HEIGHT
+        self.jumpCount2 = JUMP_HEIGHT
+        #Hitting things
+        self.hit = False
+        self.hitCount = 20
         
-        
+        #Updates the player, called after every action.
     def show(self,colour,screen):
         global STAGE
         screen.fill((255,255,255))
@@ -41,79 +54,95 @@ class Player():
         pygame.draw.rect(screen, colour, self.life2)
         pygame.display.update()
     
-    #Always falling now, cant fix one pixel being erased
+    #Always falling unless touching the stage or jumping. Need to mess around with the accelation.
     def falling(self):
         global plColor
         global STAGE
         
-        if not self.hitbox.colliderect(STAGE):
-            self.hitbox.move_ip(0,10)
+        if not self.hitbox.colliderect(STAGE) and not self.jump1:
+            self.hitbox.move_ip(0,GRAVITY)
             self.show(plColor,screen)
-        if not self.hitbox2.colliderect(STAGE):
-            self.hitbox2.move_ip(0,10)
+        if not self.hitbox2.colliderect(STAGE) and not self.jump2:
+            self.hitbox2.move_ip(0,GRAVITY)
             self.show(plColor,screen)
         
-        
+        #Resets a player if he falls off the stage and takes a life
     def reset(self):
         global plColor
         
         if self.hitbox.y >= 940:
              self.hitbox = pygame.Rect(self.x,self.y,PLAYER_HEIGHT,PLAYER_WIDTH)
-             self.life.inflate_ip(-20,0)
+             self.life.inflate_ip(-TAKE_LIFE,0)
              self.show(plColor,screen)
         if self.hitbox2.y >= 940:
              self.hitbox2 = pygame.Rect(self.x+400,self.y,PLAYER_HEIGHT,PLAYER_WIDTH)
-             self.life2.inflate_ip(-20,0)
+             self.life2.inflate_ip(-TAKE_LIFE,0)
              self.show(plColor,screen)
             
             
                 
-        
+        #Static move, can mess around with acceleration later
     def move(self):
         global plColor
             
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
-            self.hitbox.move_ip(-5,0)
+            self.hitbox.move_ip(-MOVING_SPEED,0)
             self.show(plColor,screen)
         if keys[pygame.K_d]:
-            self.hitbox.move_ip(5,0)
+            self.hitbox.move_ip(MOVING_SPEED,0)
             self.show(plColor,screen)
         
         if keys[pygame.K_LEFT]:
-            self.hitbox2.move_ip(-5,0)
+            self.hitbox2.move_ip(-MOVING_SPEED,0)
             self.show(plColor,screen)
         if keys[pygame.K_RIGHT]:
-            self.hitbox2.move_ip(5,0)
+            self.hitbox2.move_ip(MOVING_SPEED,0)
             self.show(plColor,screen)
             
     
         
-        #Doesnt work
-# =============================================================================
-#     def jump(self):
-#         global bgColor
-#         global plColor
-#         
-# 
-#         keys = pygame.key.get_pressed()
-#         if keys[pygame.K_w]:
-#             if self.y == 440:
-#                 return
-#             self.show(bgColor)
-#             self.y -= FALLING_SPEED
-#             self.show(plColor)
-#        #Doesnt work
-#     def hit(self):
-#         global screen
-#         keys = pygame.key.get_pressed()
-#         if keys[pygame.K_e]:
-#         
-#             arm = pygame.Rect(self.x+20,self.y, 15,5)
-#             pygame.draw.rect(screen,pygame.Color('green') , arm)
-#             self.hitbox.union(arm)
-# =============================================================================
+        #Static jump, can mess around with equation later. Also need to figure out how to stop more inputs?
+    def jump(self):
         
+        global plColor
+        if self.jump1:
+            if self.jumpCount >= 0:
+                self.hitbox.move_ip(0,-GRAVITY)
+                self.jumpCount -= GRAVITY
+                self.show(plColor,screen)
+            else:
+                self.jump1 = False
+                self.jumpCount = JUMP_HEIGHT
+                
+        if self.jump2:
+            if self.jumpCount2 >= 0:
+                self.hitbox2.move_ip(0,-GRAVITY)
+                self.jumpCount2 -= GRAVITY
+                self.show(plColor,screen)
+            else:
+                self.jump2 = False
+                self.jumpCount2 = JUMP_HEIGHT
+        
+                    
+                
+                
+       #Doesnt work
+    def hit(self):
+        global screen
+        
+        if self.hit:
+            if self.hitCount > 20:
+            
+                arm = pygame.Rect(self.x+20,self.y, 15,5)
+                pygame.draw.rect(screen,pygame.Color('green') , arm)
+                self.hitbox.union(arm)
+                self.hitCount -= 5
+                self.show(plColor,screen)
+            else:
+                self.hit = False
+                self.hitCount = 20
+            
     
     def getHit(self):
         pass
@@ -134,33 +163,39 @@ pygame.init()
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 
-
+#Colours, will change these later
 fgColor = pygame.Color("black")
 plColor = pygame.Color('red')
-# filling the background
+# filling the background (255,255,255) is just white
 screen.fill((255,255,255))
 
 #fill this in stage
 STAGE = pygame.Rect(400,600,WIDTH/2,HEIGHT/20)
 
-#Will change these numbers later to variables
+#Initialise our players
 PLAYER = Player(PLAYER1_X,PLAYER1_Y)
 PLAYER.show(plColor,screen)
 
-#PLAYER.showLife(plColor)
-#PLAYER2.showLife(plColor)
 while True:
+    #all our events, might be worth putting into a method later, leave for now.
     e = pygame.event.poll()
     if e.type == pygame.QUIT:
         break
+    elif e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_w:
+                PLAYER.jump1 = True
+            if e.key == pygame.K_UP:
+                PLAYER.jump2 = True
+            if e.key == pygame.K_e:
+                PLAYER.hit = True
+              
     clock.tick(FPS)
-    # visualise the changes you just made
+    # All the actions
     PLAYER.move()
+    PLAYER.jump()
     PLAYER.falling()
     PLAYER.reset()
+    #Update the display
     pygame.display.flip()
-    
-    #PLAYER2.falling()
-    #PLAYER.jump()
 
 pygame.quit() 
